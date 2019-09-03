a757c46828975f35643c7ca4391e4cb35c442236
尹玉慧 <yuhui@yinyuhuideMacBook-Pro.local>
2019-09-04 00:05:40 +0800

    header 基本布局

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import { 
     HeaderWrapper,
     Logo,
-    Nav
+    Nav,
+    NavItem,
+    NavSearch,
+    Addition,
+    Button,
 } from './style'
 
 class Header extends Component {
@@ -12,10 +16,17 @@ class Header extends Component {
                 <HeaderWrapper>
                     <Logo href="/" />
                     <Nav>
-
+                        <NavItem className="left active">首页</NavItem>
+                        <NavItem className="left">下载APP</NavItem>
+                        <NavItem className="right">登录</NavItem>
+                        <NavItem className="right">Aa</NavItem>
+                        <NavSearch></NavSearch>
                     </Nav>
                 </HeaderWrapper>
-                
+                <Addition>
+                    <Button className="writing">写文章</Button>
+                    <Button className="reg">注册</Button>
+                </Addition>
             </div>
         )
     }
